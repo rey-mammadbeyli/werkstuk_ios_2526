956ca839af9a5a07d93e7673fd271533f130b1a3
rey-mammadbeyli <rey.mammadbeyli@student.ehb.be>
2026-08-16 13:50:09 +0200

    Removal of content view

--- Weatherify/WeatherifyApp.swift
+++ Weatherify/WeatherifyApp.swift
@@ -2,12 +2,14 @@ import SwiftUI
 
 @main
 struct WeatherifyApp: App {
-    let persistenceController = PersistenceController.shared
+    @StateObject private var viewModel = WeatherViewModel()
+    private let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            HomeView()
+                .environmentObject(viewModel)
+                .environment(\.managedObjectContext, persistenceController.context)
         }
     }
 }
